a6e365df8b1ca62459103ddb003417ce2d285df1
codewriter7 <51426100+codewriter7@users.noreply.github.com>
2019-06-05 22:24:51 -0700

    Update designs.js
    Remove code that checks max grid dimensions and add this functionality to the index.html document.
    Update the comments style to conform with standards.

--- designs.js
+++ designs.js
@@ -1,14 +1,18 @@
 const sizePicker = document.getElementById('sizePicker');
 const canvas = document.getElementById('pixelCanvas');
 
-// Remove canvas from the window
+/**
+* Remove canvas from the window
+*/
 function clearTable() {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
 }
 
-// Reset the form and call clearTable
+/**
+* Reset the form and call clearTable
+*/
 function resetForm() {
     sizePicker.reset();
     if (canvas.firstChild) { // Check if a canvas already exists
@@ -16,7 +20,9 @@ function resetForm() {
     }
 }
 
-// Set pixel color to currently selected color picker color
+/**
+* Set pixel color to currently selected color picker color
+*/
 function colorPixel(event) {
     if (event.target.nodeName === 'TD') {
         const square = event.target;
@@ -25,19 +31,37 @@ function colorPixel(event) {
     }
 }
 
-// Draw the canvas
+/**
+* Draw the canvas
+*/
 function makeGrid(height, width) {
-    if (height > 50 || width > 50) {
-        alert('Maximun grid dimension 50 x 50');
-    } else {
-        for (let x = 1; x <= height; x++) {
-            const newTr = document.createElement('tr');
-            for (let y = 1; y <= width; y++) {
-                const newTd = document.createElement('td');
-                newTr.appendChild(newTd);
-            }
-            canvas.appendChild(newTr);
+    for (let x = 1; x <= height; x++) {
+        const newTr = document.createElement('tr');
+        for (let y = 1; y <= width; y++) {
+            const newTd = document.createElement('td');
+            newTr.appendChild(newTd);
         }
+        canvas.appendChild(newTr);
+    }
+}
+
+/**
+* Collect grid dimensions on click and call makeGrid
+*/
+function submitForm(event) {
+    if (canvas.firstChild) { // Check if a canvas already exists
+        clearTable();
+    }
+    const gridHeight = sizePicker.elements[0].value;
+    const gridWidth = sizePicker.elements[1].value;
+    event.preventDefault();
+    makeGrid(gridHeight, gridWidth);
+}
+
+sizePicker.addEventListener('submit', submitForm);
+
+canvas.addEventListener('click', colorPixel);
+
     }
 }
 
